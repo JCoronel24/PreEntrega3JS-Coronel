0938d5310dd41a75579fd4ce05eb3e3b914eb689
JCoronel24 <jhostincoronel91@outlook.com>
2024-05-23 18:57:15 -0400

    final del proyecto

--- script.js
+++ script.js
@@ -1,135 +1,150 @@
-let camiones = [
-    {
-        marca: 'Mercedes-Benz',
-        modelo: 'Actros',
-        url: 'https://panel.construproductos.com/images/thumbnails/65ef8047bad26.jpg',
-        año: 2019
-    },
-    {
-        marca: 'Volvo',
-        modelo: 'FH',
-        url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPeeSVZyKSVhLc6gS07VEqcwFngCbEaawtGOLZ53jgmg&s',
-        año: 2018
-    },
-    {
-        marca: 'Scania',
-        modelo: 'S500',
-        url : 'https://img.remediosdigitales.com/809da4/toyota-camion-fcev-1/200_200.jpeg',
-        año: 2020
-    },
-    {
-        marca: 'Mercedes-Benz',
-        modelo: 'Atego',
-        url : 'https://es-data.manualslib.com/product-images/5a3/212078/200x200/komatsu-hm400-3m0-camiones.jpg',
-        año: 2017
-    }
-];
+let camiones = []
 
+function getCamiones() {
+    fetch("./camiones.json")
+        .then(response => response.json())
+        .then((data) => {
+            // Guardar camiones predeterminados en localStorage si no existen
+            if (!localStorage.getItem('camiones')) {
+                localStorage.setItem('camiones', JSON.stringify(data));
+            }
+            camiones = JSON.parse(localStorage.getItem('camiones')) || data;
+            mostrarCamiones(camiones);
+        })
+        .catch(error => {
+            console.error('Error al cargar camiones.json:', error);
+        });
+}
+
+
+getCamiones()
 // Obtener los datos guardados del almacenamiento local al cargar la página
 window.addEventListener('DOMContentLoaded', () => {
     // Obtener los datos guardados o inicializarlos si no hay ninguno
-    let storedCamiones = JSON.parse(localStorage.getItem('camiones')) || camiones;
+    let storedCamiones = JSON.parse(localStorage.getItem('camiones')) || camiones
 
-    mostrarCamiones(storedCamiones);
-});
+    mostrarCamiones(storedCamiones)
+})
 
 // Elementos del DOM
-let containerCamiones = document.getElementById('Camiones'); 
-let inputModeloCamion = document.getElementById('modeloCamion');
-let inputUrlCamion = document.getElementById('urlCamion');
-let inputMarcaCamion = document.getElementById('marcaCamion');
-let inputAnoCamion = document.getElementById('anoCamion');
-let button = document.getElementById('agregar');
+let containerCamiones = document.getElementById('Camiones')
+let inputModeloCamion = document.getElementById('modeloCamion')
+let inputUrlCamion = document.getElementById('urlCamion')
+let inputMarcaCamion = document.getElementById('marcaCamion')
+let inputAnoCamion = document.getElementById('anoCamion')
+let button = document.getElementById('agregar')
 
 // Event listener para el botón de agregar
-button.addEventListener("click", agregarCamion);
+button.addEventListener("click", agregarCamion)
+
+// Función para mostrar un toast
+function mostrarToast(mensaje, tipo) {
+    Toastify({
+        text: mensaje,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        backgroundColor: tipo === 'error' ? "#FF0000" : "#00C851",
+        stopOnFocus: true
+    }).showToast()
+}
 
 // Función para agregar un camión
 function agregarCamion () {
-    let camion = {
-        marca: inputMarcaCamion.value,
-        modelo: inputModeloCamion.value,
-        url: inputUrlCamion.value,
-        año: inputAnoCamion.value
-    }
+    if (inputModeloCamion.value && inputUrlCamion.value && inputMarcaCamion.value && inputAnoCamion.value) {
+        let camion = {
+            marca: inputMarcaCamion.value,
+            modelo: inputModeloCamion.value,
+            url: inputUrlCamion.value,
+            año: inputAnoCamion.value
+        }
 
     // Agregar el camión a la lista
-    camiones.push(camion);
+    camiones.push(camion)
 
     // Guardar los camiones en el almacenamiento local
-    localStorage.setItem('camiones', JSON.stringify(camiones));
+    localStorage.setItem('camiones', JSON.stringify(camiones))
 
     // Limpiar inputs
-    inputModeloCamion.value = '';
-    inputUrlCamion.value = '';
-    inputMarcaCamion.value = '';
-    inputAnoCamion.value = '';
+    inputModeloCamion.value = ''
+    inputUrlCamion.value = ''
+    inputMarcaCamion.value = ''
+    inputAnoCamion.value = ''
 
     // Actualizar visualización camiones
-    mostrarCamiones(camiones);
+    mostrarCamiones(camiones)
+
+    // Mostrar mensaje de éxito
+    mostrarToast('Camión agregado exitosamente!', 'success')
+    }
+    else {
+    // Mostrar mensaje de error
+        mostrarToast('Por favor, llene todos los campos', 'error')
+    }
 }
 
 // Función para mostrar los camiones
 function mostrarCamiones(camiones) {
-    containerCamiones.innerHTML = '';
+    containerCamiones.innerHTML = ''
 
     camiones.forEach(camion => {
-        const camionElemento = document.createElement('div');
-        camionElemento.classList.add('camion');
+        const camionElemento = document.createElement('div')
+        camionElemento.classList.add('camion')
         camionElemento.innerHTML = `
             <h2>${camion.modelo}</h2>
             <img src=${camion.url}></img>
             <p>Marca: ${camion.marca}</p>
             <p>Año: ${camion.año}</p>
             <button id=${camion.modelo}>Eliminar</button>
-        `;
-        containerCamiones.appendChild(camionElemento);
-    });
+        `
+        containerCamiones.appendChild(camionElemento)
+    })
 
-    actualizarBotonesEliminar();
+    // Actualizar botones de eliminar
+    actualizarBotonesEliminar()
 }
 
 // Función para filtrar los camiones
 function filtrarCamiones() {
-    let filtro = filtroCamionesInput.value.trim().toLowerCase();
+    let filtro = filtroCamionesInput.value.trim().toLowerCase()
 
     // Filtrar camiones basado en el filtro
     let camionesFiltrados = camiones.filter(camion => {
-        let modeloCamion = camion.modelo.toLowerCase();
-        let marcaCamion = camion.marca.toLowerCase();
-        let anoCamion = camion.año.toString(); 
+        let modeloCamion = camion.modelo.toLowerCase()
+        let marcaCamion = camion.marca.toLowerCase()
+        let anoCamion = camion.año.toString();
 
-        return modeloCamion.includes(filtro) || marcaCamion.includes(filtro) || anoCamion.includes(filtro);
-    });
+        return modeloCamion.includes(filtro) || marcaCamion.includes(filtro) || anoCamion.includes(filtro)
+    })
 
     // Mostrar los camiones filtrados
-    mostrarCamiones(camionesFiltrados);
+    mostrarCamiones(camionesFiltrados)
 }
 
-let filtroCamionesInput = document.getElementById('filtroCamiones');
-filtroCamionesInput.addEventListener('input', filtrarCamiones);
+let filtroCamionesInput = document.getElementById('filtroCamiones')
+filtroCamionesInput.addEventListener('input', filtrarCamiones)
+
+// Listener para el contenedor de camiones
+containerCamiones.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        eliminarCamion(event);
+    }
+})
 
 // Función para eliminar un camión
 function eliminarCamion(event) {
-    let modelo = event.target.id;
+    let modelo = event.target.id
 
     // Filtrar los camiones para eliminar el camión correspondiente
-    camiones = camiones.filter(camion => camion.modelo !== modelo);
+    camiones = camiones.filter(camion => camion.modelo !== modelo)
 
     // Guardar los camiones actualizados en el almacenamiento local
-    localStorage.setItem('camiones', JSON.stringify(camiones));
+    localStorage.setItem('camiones', JSON.stringify(camiones))
 
     // Actualizar visualización camiones
-    mostrarCamiones(camiones);
+    mostrarCamiones(camiones)
 }
 
-// Función para actualizar los eventos de los botones eliminar
-function actualizarBotonesEliminar() {
-    let botonesEliminar = document.querySelectorAll('button');
-    botonesEliminar.forEach(boton => {
-        boton.addEventListener('click', eliminarCamion);
-    });
-}
 
 
 
